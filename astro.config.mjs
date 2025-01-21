8bee2e5cea9705fcd6bec0f337becf1b2f116435
sverde <sverde@autentia.com>
2025-01-21 11:51:38 +0100

    First commit :)

---
+++ astro.config.mjs
@@ -0,0 +1,25 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import remarkToc from "remark-toc";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
+	markdown: {
+		remarkPlugins: [
+		  remarkMath,
+		  remarkToc
+		],
+		rehypePlugins: [rehypeKatex],
+		shikiConfig: {
+		  // For more themes, visit https://shiki.style/themes
+		  themes: { light: "catppuccin-latte", dark: "night-owl" },
+		  wrap: true,
+		},
+	  },
+});
