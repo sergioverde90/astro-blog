476e04f58bf950accb0babd0fe6fe4d7115b032f
sverde <sverde@autentia.com>
2025-02-21 20:01:45 +0100

    Removed remark-toc & remark-collapse

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import remarkToc from "remark-toc";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import { rehypeHeadingIds } from '@astrojs/markdown-remark';
-import remarkCollapse from "remark-collapse";
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,10 +14,7 @@ export default defineConfig({
 		'/' : '/blog/1'
 	},
 	markdown: {
-		remarkPlugins: [ remarkMath, remarkToc, [remarkCollapse,
-			{
-			  test: "Table of Contents",
-			}]],
+		remarkPlugins: [ remarkMath],
 		rehypePlugins: [ rehypeKatex, rehypeHeadingIds],
 		shikiConfig: {
 		  themes: { light: "material-theme", dark: "night-owl" },
